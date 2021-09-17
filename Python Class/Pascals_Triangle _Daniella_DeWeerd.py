15779c39d28a572b7a2ac81501ebcc739e1522a9
DaniellaDeWeerd <51303500+DaniellaDeWeerd@users.noreply.github.com>
2021-09-17 02:10:47 -0400

    Pascals new one added

---
+++ Python Class/Pascals_Triangle _Daniella_DeWeerd.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 17 00:54:11 2021
+
+@author: Daniella
+"""
+
+class binomial_coefficients: # class to find binomial coeeficients using Pascal's rule
+    def get_n(self,n): # find row
+        global row #create list to contain row that can be used in other methods
+        row = [] #initialize empty row list
+        num_of_k = n + 1 #figure out how many times k will have to be found
+        for k in range(num_of_k): #run the method to find k that many times
+            number = self.get_nk(n, k) #find k in the n,k position
+            row.append(number) #add result to row list
+        return row
+    
+    def get_nk(self,n,k): #find k for certain row
+        global row #making it able to look at that global row variable
+        if (k == 0): #if it is first number in row, it is 1
+            return 1
+        elif (k == n): #if it is last number in row, it is 1
+            return 1
+        else: #if not the first or last number:
+            previous_num = row[k-1] #find previous number in row
+            new_num = int(previous_num * ((n+1-k)/k)) #run formula to find the next number in that row
+            return new_num #return that number
+            
+    def save_pt(self,n,file_name = 'pascal_triangle.txt'): #find n number of rows and write the resulting triangle to a particular file
+        global row #again ensuring access to the gloabl row (list)
+        triangle ="" #initializing triangle string
+        for num in range(n): #for each row until you hit the row's wanted
+            spacing = n * (round(n/10)*3) #This line is for centering the numbers in the file. 
+            row = self.get_n(num) #get row n
+            string = self.listToString(row) #turn the row list into a string of the elements seperated by a space
+            string = string.center(spacing) #center that string
+            triangle += string #add it to the triangle string
+            triangle += "\n" #add a new line for the next row
+        print(triangle) #for checking purposes without looking at the file
+        f = open(file_name, "w") #open string to file (will overwrite file if it already exists)
+        f.write(triangle) #write triangle string to file
+        f.close #close file
+    
+    def listToString(self, list1): #turn list into string
+        string = "" #initialize string
+        for element in list1: #for each element:
+            string += str(element) #add the element to the string
+            string += " " #add space behind it
+        return string #return the completed string
+        
+def main(): #all for testing purposes only
+        user_input = input("Please type how many rows you would like your triangle to have.\n")
+        global how_far
+        how_far = int(user_input)
+        BC = binomial_coefficients()
+        BC.save_pt(how_far)   
+
+if __name__ == "__main__":
+    main()
+            
+            
+                
--- Python Class/pascals triangle recursion.py
+++ Python Class/pascals triangle recursion.py
@@ -5,47 +5,72 @@ Created on Thu Sep 16 13:26:15 2021
 @author: danie
 """
 
-first_row_for_use = [0,1,0]
-first_row_for_print = [1]
+
+first_row = [1]
+second_row = [1,1]
 last_row = []
 current_row=[]
+how_far = 0
 
-def pascals_triangle(how_far):
+def listToString(list1): 
+    string = "" 
+    
+    for element in list1: 
+        string += str(element)
+        string += " "
+    return string 
+
+def pascals_triangle():
     global first_row_for_print
     global first_row_for_use
     global last_row
+    global how_far
+    spacing = how_far * (round(how_far/10)*3)
     i = 0
     while i <= how_far:
         if len(last_row) == 0:
-            print('{:^24s}'.format(str(*first_row_for_print)))
-            last_row = first_row_for_use
+            string = listToString(first_row)
+            print(string.center(spacing))
+            last_row = first_row.copy()
+        elif len(last_row) == 1:
+            string = listToString(second_row)
+            print(string.center(spacing))
+            last_row = second_row.copy()
         else:
             calculate_new_row()
-            print_new_row()
         i = i + 1
 
 def calculate_new_row():
     global last_row
     global current_row
     current_row = []
+    current_row.append(1)
     last_row_number = len(last_row)
-    i = 2
-    while i <= last_row_number:
-        number_1 = last_row[i-2]
-        number_2 = last_row[i-1]
-        current_row.append(number_1 + number_2)
-        i = i + 1
+    i = 0
+    while i < (last_row_number - 1):
+        number_1 = last_row[i]
+        if number_1 == (last_row_number):
+            i = i + 1
+        else:
+            number_2 = last_row[i+1]
+            current_row.append(number_1 + number_2)
+            i = i + 1
+    current_row.append(1)
+    print_new_row()
     last_row = current_row.copy()
 
 def print_new_row():
     global current_row
-    string = str(current_row)
-    print('{:^24s}'.format(string))
+    global how_far
+    spacing = how_far * (round(how_far/10)*3)
+    string = listToString(current_row)
+    print(string.center(spacing))
 
 def main():
-    user_input = input("Please type how many rows you would like your triangle to have.")
-    user_input = int(user_input)
-    pascals_triangle(user_input)
+    user_input = input("Please type how many rows you would like your triangle to have.\n")
+    global how_far
+    how_far = int(user_input)
+    pascals_triangle()
         
 if __name__ == "__main__":
     main()
